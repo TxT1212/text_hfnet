f1bdc6904690103fcb09b3334ecc04ed9a21ef61
TxT1212 <18868108895@163.com>
2021-07-08 10:56:15 +0800

    update(text3d_location): 增加了文字区域expand搜索范围

--- locate_text_3dposition.py
+++ locate_text_3dposition.py
@@ -9,56 +9,61 @@ import argparse
 from read_write_model import *
 import math
 
+
 def load_ocr_result(result_path):
     result = np.load(result_path, allow_pickle=True)
     txts = [line[1][0] for line in result]
-    scores = [line[1][1] for line in result]    
+    scores = [line[1][1] for line in result]
     boxes = [line[0] for line in result]
     for line in result:
         print(line)
     return boxes, txts, scores
 
 
-# 
-def choose_text_points2d(img, box):
-    left = np.min(box[:, 0, 0])
-    right = np.max(box[:, 0, 0])
-    down = np.min(box[:, 0, 1])
-    up = np.max(box[:, 0, 1])
+#
+def choose_text_points2d(img, box, expand=1):
+    left = np.min(box[:, 0, 0]) * expand
+    right = np.max(box[:, 0, 0]) * expand
+    down = np.min(box[:, 0, 1]) * expand
+    up = np.max(box[:, 0, 1]) * expand
     point3D_ids_in = []
     for xy, point3D_id in zip(img.xys, img.point3D_ids):
-        if(point3D_id!=-1 and xy[0]>left and xy[0]<right and xy[1]>down and xy[1]<up):
+        if(point3D_id != -1 and xy[0] > left and xy[0] < right and xy[1] > down and xy[1] < up):
             point3D_ids_in.append(point3D_id)
     return point3D_ids_in
 
 
-
 def main(model_path, ocr_output_path):
-    
+
     images = read_images_binary(os.path.join(model_path, "images.bin"))
     points3D = read_points3d_binary(os.path.join(model_path, "points3D.bin"))
     print(len(images))
     dict_test2xyz = {}
     for image_id in images:
         print(images[image_id].name)
-        boxes, txts, scores = load_ocr_result(ocr_output_path + "/" + images[image_id].name + ".ocr_result.npy")
+        boxes, txts, scores = load_ocr_result(
+            ocr_output_path + "/" + images[image_id].name + ".ocr_result.npy")
         if(len(boxes) == 0):
             continue
         for box_id in range(len(boxes)):
             if scores is not None and (scores[box_id] < 0.5 or math.isnan(scores[box_id])):
                 continue
-            box = np.reshape(np.array(boxes[box_id]), [-1, 1, 2]).astype(np.int64)
-            points3d_ids_in = choose_text_points2d(images[image_id], box)
+            box = np.reshape(
+                np.array(boxes[box_id]), [-1, 1, 2]).astype(np.int64)
+            points3d_ids_in = choose_text_points2d(images[image_id], box, 1)
             xyzs = []
             for points3d_id_in in points3d_ids_in:
                 xyz = points3D[points3d_id_in].xyz
                 xyzs.append(xyz)
-            if(len(xyzs)>0):
+            if(len(xyzs) > 0):
                 xyzs = np.array(xyzs).reshape(-1, 3)
-                np.save(ocr_output_path + "/" + images[image_id].name + str(box_id) + "_cloud3d.npy", xyzs)
+                np.save(ocr_output_path + "/" +
+                        images[image_id].name + str(box_id) + "_cloud3d.npy", xyzs)
                 xyz_average = np.average(xyzs, axis=0)
-                dict_test2xyz[images[image_id].name + str(box_id)] = xyz_average
+                dict_test2xyz[images[image_id].name +
+                              str(box_id)] = xyz_average
     np.save(ocr_output_path + "/dict_test2xyz.npy", dict_test2xyz)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
