15de396f98758f477a307049903a21e87bc7bf79
TxT1212 <18868108895@163.com>
2021-07-15 19:56:50 +0800

    update(@find_text_same_3d): 找到對應相同3d区域的2d文本，初步可运行版本

--- find_text_same_3d.py
+++ find_text_same_3d.py
@@ -1,15 +1,27 @@
 import numpy as np
 from numpy.core.defchararray import array
+import open3d as o3d
+from read_write_model import *
 
 areas = np.load('/home/ezxr/Documents/wxc/pic_ocr_flip/dict_test2xyz.npy', allow_pickle=True)
-
+ocr_outpath = '/home/ezxr/Documents/wxc/pic_ocr_flip/'
+model_path = '/home/ezxr/Documents/wxc/f2_aligned_to_f1_gz/0/'
 dict_test2xyz = areas.item()
+images = read_images_binary(os.path.join(model_path, "images.bin"))
 # print(type(dict_test2xyz))
 items_3d = []
-d_threshold = 0.4
-for test_xyz in dict_test2xyz:
-    xyz = dict_test2xyz[test_xyz]
-    test = test_xyz
+d_threshold = 1
+count_point_nums = np.zeros(20)
+for image_name in dict_test2xyz:
+    xyz = dict_test2xyz[image_name]
+    points = np.load(ocr_outpath+image_name+"_cloud3d.npy")
+    # print(points.shape[0])
+    if(points.shape[0]<5):
+        count_point_nums[points.shape[0]-1] += 1
+    else:
+        count_point_nums[19] += 1
+
+
     # print(type(xyz))
     flag_found_same = False
     for item_3d in items_3d:
@@ -18,10 +30,11 @@ for test_xyz in dict_test2xyz:
             continue
         else:
             flag_found_same = True
-            item_3d[1].append(test)
+            item_3d[1].append(image_name)
             break
     if(not flag_found_same):
-        items_3d.append([xyz, [test]])
-        
+        items_3d.append([xyz, [image_name]])
+    # print(count_point_nums)
 for item_3d in items_3d:
-    print(item_3d)
+    for orc2d_name in item_3d[1]:
+        print(ocr_outpath + orc2d_name, ocr_outpath + item_3d[1][0])
