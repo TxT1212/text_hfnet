57341d76aad31600aab3df951a3aa251d7691788
TxT1212 <18868108895@163.com>
2021-07-19 15:43:02 +0800

    update(@find_text_same_3d): 找到点不足的图片。
    这是更新前最后一版本，之后直接读取ocr区域的所有信息进行分类，不再需要读取colmap生成的image.bin

--- find_text_same_3d.py
+++ find_text_same_3d.py
@@ -3,7 +3,8 @@ from numpy.core.defchararray import array
 import open3d as o3d
 from read_write_model import *
 
-areas = np.load('/home/ezxr/Documents/wxc/pic_ocr_flip/dict_test2xyz.npy', allow_pickle=True)
+areas = np.load(
+    '/home/ezxr/Documents/wxc/pic_ocr_flip/dict_test2xyz.npy', allow_pickle=True)
 ocr_outpath = '/home/ezxr/Documents/wxc/pic_ocr_flip/'
 model_path = '/home/ezxr/Documents/wxc/f2_aligned_to_f1_gz/0/'
 dict_test2xyz = areas.item()
@@ -12,20 +13,22 @@ images = read_images_binary(os.path.join(model_path, "images.bin"))
 items_3d = []
 d_threshold = 1
 count_point_nums = np.zeros(20)
+image_names = [[], [], [], []]
+
 for image_name in dict_test2xyz:
     xyz = dict_test2xyz[image_name]
     points = np.load(ocr_outpath+image_name+"_cloud3d.npy")
     # print(points.shape[0])
-    if(points.shape[0]<5):
+    if(points.shape[0] < 5):
         count_point_nums[points.shape[0]-1] += 1
+        image_names[points.shape[0]-1].append(image_name)
     else:
         count_point_nums[19] += 1
-
 
     # print(type(xyz))
     flag_found_same = False
     for item_3d in items_3d:
-        dist = np.linalg.norm(xyz - item_3d[0]) 
+        dist = np.linalg.norm(xyz - item_3d[0])
         if(dist > d_threshold):
             continue
         else:
@@ -35,6 +38,11 @@ for image_name in dict_test2xyz:
     if(not flag_found_same):
         items_3d.append([xyz, [image_name]])
     # print(count_point_nums)
-for item_3d in items_3d:
-    for orc2d_name in item_3d[1]:
-        print(ocr_outpath + orc2d_name, ocr_outpath + item_3d[1][0])
+# for item_3d in items_3d:
+#     for orc2d_name in item_3d[1]:
+#         print(ocr_outpath + orc2d_name, ocr_outpath + item_3d[1][0])
+for i in range(0,4):
+    print("points: ", i+1)
+    for img in image_names[i]:
+        print(ocr_outpath + img + ".jpg")
+    
