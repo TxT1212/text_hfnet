73c7ef26522f4f03334d6dcab4dd4981044d70da
TxT1212 <18868108895@163.com>
2021-07-21 16:17:53 +0800

    update(locate_text_3d_position): 叉乘法直接判断点是否在框内，不再扩大框和uv坐标对其

--- locate_text_3dposition.py
+++ locate_text_3dposition.py
@@ -6,6 +6,8 @@
 ####################################################
 import numpy as np
 import argparse
+
+from numpy.core.numeric import cross
 from read_write_model import *
 import math
 
@@ -15,20 +17,29 @@ def load_ocr_result(result_path):
     txts = [line[1][0] for line in result]
     scores = [line[1][1] for line in result]
     boxes = [line[0] for line in result]
-    for line in result:
-        print(line)
+    # for line in result:
+    #     print(line)
     return boxes, txts, scores
 
 
 #
-def choose_text_points2d(img, box, expand=1):
-    left = np.min(box[:, 0, 0]) * expand
-    right = np.max(box[:, 0, 0]) * expand
-    down = np.min(box[:, 0, 1]) * expand
-    up = np.max(box[:, 0, 1]) * expand
+def if_point_in_box(point, box, box_diff):
+    cross_result = np.cross(box - point, box_diff) 
+    # print("box ", box)
+    # print("point ", point)
+    return np.all(cross_result > 0) or np.all(cross_result < 0)
+
+def choose_text_points2d(img, box):
+    # left = np.min(box[:, 0, 0]) * expand
+    # right = np.max(box[:, 0, 0]) * expand
+    # down = np.min(box[:, 0, 1]) * expand
+    # up = np.max(box[:, 0, 1]) * expand
+    box_diff = np.diff(box,axis = 0,prepend=box[-1:,:])
     point3D_ids_in = []
     for xy, point3D_id in zip(img.xys, img.point3D_ids):
-        if(point3D_id != -1 and xy[0] > left and xy[0] < right and xy[1] > down and xy[1] < up):
+        if(point3D_id == -1 ):
+            continue
+        if(if_point_in_box(xy, box, box_diff)):
             point3D_ids_in.append(point3D_id)
     return point3D_ids_in
 
@@ -37,10 +48,10 @@ def main(model_path, ocr_output_path):
 
     images = read_images_binary(os.path.join(model_path, "images.bin"))
     points3D = read_points3d_binary(os.path.join(model_path, "points3D.bin"))
-    print(len(images))
+    # print(len(images))
     dict_test2xyz = {}
     for image_id in images:
-        print(images[image_id].name)
+        # print(images[image_id].name)
         boxes, txts, scores = load_ocr_result(
             ocr_output_path + "/" + images[image_id].name + ".ocr_result.npy")
         if(len(boxes) == 0):
@@ -49,8 +60,8 @@ def main(model_path, ocr_output_path):
             if scores is not None and (scores[box_id] < 0.5 or math.isnan(scores[box_id])):
                 continue
             box = np.reshape(
-                np.array(boxes[box_id]), [-1, 1, 2]).astype(np.int64)
-            points3d_ids_in = choose_text_points2d(images[image_id], box, 1)
+                np.array(boxes[box_id]), [-1, 1, 2]).astype(np.int64).squeeze()
+            points3d_ids_in = choose_text_points2d(images[image_id], box)
             xyzs = []
             for points3d_id_in in points3d_ids_in:
                 xyz = points3D[points3d_id_in].xyz
@@ -58,11 +69,14 @@ def main(model_path, ocr_output_path):
             if(len(xyzs) > 0):
                 xyzs = np.array(xyzs).reshape(-1, 3)
                 np.save(ocr_output_path + "/" +
-                        images[image_id].name + str(box_id) + "_cloud3d.npy", xyzs)
+                        images[image_id].name + str(box_id) + "_cloud3d_1.npy", xyzs)
                 xyz_average = np.average(xyzs, axis=0)
                 dict_test2xyz[images[image_id].name +
                               str(box_id)] = xyz_average
-    np.save(ocr_output_path + "/dict_test2xyz.npy", dict_test2xyz)
+            else:
+                print(ocr_output_path + "/" +
+                      images[image_id].name + str(box_id) + ".jpg")
+    np.save(ocr_output_path + "/dict_test2xyz_f1_1.npy", dict_test2xyz)
 
 
 if __name__ == '__main__':
