8e486e960a946c719e24c42a2c654f567d377630
TxT1212 <18868108895@163.com>
2021-08-28 17:08:58 +0800

    update(@pipeline): 运行脚本

--- run.sh
+++ run.sh
@@ -17,9 +17,10 @@ cd ${ws}${subFolder}/release
 # mkdir sfm_sift
 # colmap point_triangulator --Mapper.ba_refine_focal_length 0 --Mapper.ba_refine_extra_param 0 --database_path database.db --image_path ./mapping/sensors/records_data/ --input_path . --output_path sfm_sift/
 cd /home/txt/Downloads/ezxr_loc/Hierarchical-Localization/
-# python -m hloc.pipelines.naver.pipeline
-python -m hloc.pipelines.naverF4.pipeline
-python -m hloc.pipelines.naverB1.pipeline
+python -m hloc.pipelines.naver.pipeline --loc_pairs /media/txt/data2/naver/HyundaiDepartmentStore/outputs/1F/pairs-query-netvlad20.txt --results /media/txt/data2/naver/HyundaiDepartmentStore/outputs/1F/hloc_superpoint+superglue_netvlad_20.txt --loc_matches /media/txt/data2/naver/HyundaiDepartmentStore/outputs/1F/feats-superpoint-n4096-r1600_matches-superglue_pairs-query-netvlad50.h5
+# python -m hloc.pipelines.naver.pipeline --loc_pairs /home/txt/Downloads/ezxr_loc/text_hfnet/saved/logo_nn_f1.txt --results /media/txt/data2/naver/HyundaiDepartmentStore/outputs/1F/hloc_superpoint+superglue_logo_20_.txt
+# python -m hloc.pipelines.naverF4.pipeline
+# python -m hloc.pipelines.naverB1.pipeline 
 
 
 # which python
