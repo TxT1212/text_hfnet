26bf0af91e4eb96cc6d53dec0b31da31e8fc895e
TxT1212 <18868108895@163.com>
2021-08-31 16:46:42 +0800

    update(@ocr_area_extract): debug

--- pipeline/ocr_area_extract.py
+++ pipeline/ocr_area_extract.py
@@ -1,6 +1,7 @@
 from os import sysconf
 import pickle
 from typing import Pattern
+from joblib.logger import PrintTime
 import numpy as np
 import argparse
 import math
@@ -48,23 +49,52 @@ def ocr_images_cut(input_path, ocr_results, output_path):
             height_ = up - down
             center_u = (left + right) / 2
             center_v = (up + down) / 2
-            exapnd_max = min(img.shape[0] - up, left, down, img.shape[1]-right)
+            exapnd_max = min(img.shape[0], img.shape[1])
             for expand_ratio in expand_ratios:
-                exapnd_expect = max(width_, height_)*expand_ratio/2
+                exapnd_expect = max(width_, height_)*(expand_ratio + 1)
                 exapnd_actual = min(exapnd_expect, exapnd_max)
                 exapnd_actual = int(exapnd_actual)
-                left_expand = left - exapnd_actual
-                down_expand = down - exapnd_actual
-                right_expand = right + exapnd_actual
-                up_expand = up + exapnd_actual
-                image_chop = img[down_expand:up_expand +
-                                 1, left_expand:right_expand+1]
+                chop_length = exapnd_actual
+                left_expect = center_u - chop_length/2
+                right_expect = center_u + chop_length/2
+                down_expect = center_v - chop_length/2
+                up_expect = center_v + chop_length/2
+                if(down_expect < 0):
+                    up_expand = up_expect - down_expect
+                    down_expand = 0
+                elif(up_expect > img.shape[0]):
+                    down_expand = down_expect + (img.shape[0] - up_expect)
+                    up_expand = img.shape[0]
+                else:
+                    down_expand = down_expect
+                    up_expand = up_expect
+
+                if(left_expect < 0):
+                    right_expand = right_expect - left_expect
+                    left_expand = 0
+                elif(right_expect > img.shape[1]):
+                    left_expand = left_expect + (img.shape[1] - right_expect)
+                    right_expand = img.shape[1]
+                else:
+                    left_expand = left_expect
+                    right_expand = right_expect
+                down_expand = int(down_expand)
+                left_expand = int(left_expand)
+                up_expand = int(up_expand)
+                right_expand = int(right_expand)
+                image_chop = img[down_expand:up_expand, left_expand:right_expand]
 
                 # save result
                 txt = txts[i]
                 save_name = output_path + str.replace(img_path, input_path, "") + str(i) + "_txt_" + txt.replace(
                     "/", "").replace(" ", "") + "_confidence_" + str(int(scores[i]*1000)/1000.) + "_expand_" + str(expand_ratio) + ".jpg"
                 print(save_name)
+                # print(image_chop.shape)
+                # print(up_expand, down_expand, up_expect, down_expect)
+                # print(right_expand, left_expand, right_expect, left_expect)
+                # print(chop_length, right_expect - left_expect,up_expect - down_expect )
+                # print(img.shape[0], img.shape[1])
+                assert(abs(image_chop.shape[0] - image_chop.shape[1]) < 3)
                 cv2.imwrite(save_name, image_chop)
 
 
