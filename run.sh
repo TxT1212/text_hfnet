cc1aebe306632befb0ab83a73f8d502c5dd77981
TxT1212 <18868108895@163.com>
2021-08-31 16:47:46 +0800

    update(@shell): 更新脚本
    “”

--- run.sh
+++ run.sh
@@ -1,20 +1,32 @@
-###### orc 
-image_pathq=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/query_images_undistort
-ocr_pathq=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/query_images_ocr
+###### orc /
+conda activate ocr
+image_pathq=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/query_images_undistort/
+ocr_pathq=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/query_images_ocr/
 # python pipeline/padder_ocr.py --input_path $image_pathq --output_path $ocr_pathq
-# python pipeline/ocr_area_extract.py --input_path $image_pathq --output_path $ocr_pathq
+python pipeline/ocr_area_extract.py --input_path $image_pathq --output_path $ocr_pathq
 
-image_pathdb=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/training_images_undistort
-ocr_pathdb=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/training_image_ocr
+image_pathdb=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/training_images_undistort/
+ocr_pathdb=/home/ezxr/Documents/ibl_dataset_cvpr17_3852/training_image_ocr/
 # python pipeline/padder_ocr.py --input_path $image_pathdb --output_path $ocr_pathdb
-# python pipeline/ocr_area_extract.py --input_path $image_pathdb --output_path $ocr_pathdb
+python pipeline/ocr_area_extract.py --input_path $image_pathdb --output_path $ocr_pathdb
 
 desc_save_path=/home/ezxr/Downloads/ocr/text_hfnet/saved_ibl/
 
-python pipeline/hfnet.py --input_path $ocr_pathdb --output_path $desc_save_path --save_prefix db_p8 --image_suffix expand_8.jpg
-python pipeline/hfnet.py --input_path $ocr_pathq --output_path $desc_save_path --save_prefix query_p8 --image_suffix expand_8.jpg
+# hfnet
+# python pipeline/hfnet.py --input_path $ocr_pathdb --output_path $desc_save_path --save_prefix db_p8_hfnet --image_suffix expand_8.jpg
+# python pipeline/hfnet.py --input_path $ocr_pathq --output_path $desc_save_path --save_prefix query_p8_hfnet --image_suffix expand_8.jpg
+# hfnet_pairs_list=saved_ibl/hfnet_pairs_list.txt
+# python pipeline/hfnet_nn.py --input_path $desc_save_path --save_query_prefix query_p8_hfnet --save_db_prefix db_p8_hfnet >> $hfnet_pairs_list
 
-# subFolder=4F
+# # paddleClas
+# paddle_clas_path=/home/ezxr/Downloads/PaddleClas
+# cd $paddle_clas_path
+# python python/get_descriptor.py  -c configs/inference_logo.yaml IndexProcess.gallery_features_outpath="$desc_save_path/db_logo_globaldesc.npy" IndexProcess.gallery_images_outpath="$desc_save_path/db_logo_globalindex.npy" IndexProcess.image_root="$ocr_pathdb" 
+# python python/get_descriptor.py  -c configs/inference_logo.yaml IndexProcess.gallery_features_outpath="$desc_save_path/query_logo_globaldesc.npy" IndexProcess.gallery_images_outpath="$desc_save_path/query_logo_globalindex.npy" IndexProcess.image_root="$ocr_pathq" 
+# cd - # 回到当前文件夹
+# paddle_clas_pairs_list=saved_ibl/paddle_clas_pairs_list.txt
+# python pipeline/hfnet_nn.py --input_path $desc_save_path --save_query_prefix query_logo --save_db_prefix query_logo >> $paddle_clas_pairs_list --threshold 0.85
+# subFolder=4F 
 # ws=/media/txt/data2/naver/HyundaiDepartmentStore/
 # # kapture_export_colmap.py -i ${ws}${subFolder}/release/mapping -db ${ws}${subFolder}/release/database.db -txt ${ws}${subFolder}/release/
 # cd ${ws}${subFolder}/release
@@ -37,6 +49,3 @@ python pipeline/hfnet.py --input_path $ocr_pathq --output_path $desc_save_path -
 # python -m hloc.pipelines.naverB1.pipeline
 
 
-# which python
-# python pipeline/padder_ocr.py --output_path /media/txt/data2/data2/naver/ocr/ --input_path /media/txt/data2/data2/naver/HyundaiDepartmentStore
-# python pipeline/ocr_area_extract.py --output_path /media/txt/data2/data2/naver/ocr/ --input_path /media/txt/data2/data2/naver/HyundaiDepartmentStore/
